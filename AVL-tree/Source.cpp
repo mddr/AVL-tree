8b031809db576e3eb77348d21d16a6b8354d878e
Paweł Jacewicz <pawel138@gmail.com>
2017-11-24 19:23:51 +0100

    Adding new element and printing tree works properly now

--- AVL-tree/Source.cpp
+++ AVL-tree/Source.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip>
+#include <string>
 using namespace std;
 
 struct Node {
@@ -9,23 +11,56 @@ struct Node {
 };
 
 void add(string newIntegerPart, string newDecimalPart, Node* root) {
-	while (root != NULL) {
-		if (newIntegerPart > root->integerPart ||
-			(newIntegerPart == root->integerPart && newDecimalPart > root->decimalPart)) {
-			root = root->right;
+	if (root != NULL) {
+		if (newIntegerPart > root->integerPart || (newIntegerPart == root->integerPart && newDecimalPart > root->decimalPart)) {
+			if (root->right != NULL)
+				add(newIntegerPart, newDecimalPart, root->right);
+			else {
+				root->right = new Node;
+				root->right->integerPart = newIntegerPart;
+				root->right->decimalPart = newDecimalPart;
+				root->right->right = NULL;
+				root->right->left = NULL;
+			}
 		}
 		else {
-			root = root->left;
+			if (root->left != NULL)
+				add(newIntegerPart, newDecimalPart, root->left);
+			else {
+				root->left = new Node;
+				root->left->integerPart = newIntegerPart;
+				root->left->decimalPart = newDecimalPart;
+				root->left->right = NULL;
+				root->left->left = NULL;
+			}
 		}
 	}
-	root->integerPart = newIntegerPart;
-	root->decimalPart = newDecimalPart;
-	root->right = NULL;
-	root->left = NULL;
+}
+
+void printTree(Node* node, int indent = 0) {
+	if (node != NULL) {
+		cout << setw(indent) << (node->integerPart) << '.' << (node->decimalPart) << endl;
+		if (node->left != NULL)
+			printTree(node->left, indent + 4);
+		if (node->right != NULL)
+			printTree(node->right, indent + 4);
+	}
 }
 
 
 int main() {
-	cout << "hello world";
-	
+	Node* root = new Node;
+	root->left = NULL;
+	root->right = NULL;
+	root->decimalPart = "6";
+	root->integerPart = "1";
+	add("1", "8", root);
+	add("1", "4", root);
+	add("1", "9", root);
+	add("1", "7", root);
+	add("1", "6", root);
+	add("1", "0", root);
+
+	printTree(root);
+	getchar();
 }
