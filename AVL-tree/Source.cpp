b1088ab7f99861aa76d0a4f1028ea3beba2c9254
Paweł Jacewicz <pawel138@gmail.com>
2017-11-24 18:28:10 +0100

    Node structure and 'add' function

--- AVL-tree/Source.cpp
+++ AVL-tree/Source.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
 
+struct Node {
+	string integerPart;
+	string decimalPart;
+	Node* right;
+	Node* left;
+};
+
+void add(string newIntegerPart, string newDecimalPart, Node* root) {
+	while (root != NULL) {
+		if (newIntegerPart > root->integerPart ||
+			(newIntegerPart == root->integerPart && newDecimalPart > root->decimalPart)) {
+			root = root->right;
+		}
+		else {
+			root = root->left;
+		}
+	}
+	root->integerPart = newIntegerPart;
+	root->decimalPart = newDecimalPart;
+	root->right = NULL;
+	root->left = NULL;
+}
+
+
 int main() {
 	cout << "hello world";
 	
